fd5d85933e38979447bb853410a0f5e8d9856ee3
edulazaro <eduzro@gmail.com>
2020-07-03 02:25:39 +0100

    Updates

---
+++ precache-manifest.4cddcf09c97bb51e7d1cc4a9ea430cbf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4b70a10ebd27737837852642eee0b87",
+    "url": "/tutorial-react/index.html"
+  },
+  {
+    "revision": "68fde10162ad9ba6d04d",
+    "url": "/tutorial-react/static/css/main.794000d0.chunk.css"
+  },
+  {
+    "revision": "ffcf453ea9eab41f947d",
+    "url": "/tutorial-react/static/js/2.230b6b81.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tutorial-react/static/js/2.230b6b81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68fde10162ad9ba6d04d",
+    "url": "/tutorial-react/static/js/main.78e1d9b6.chunk.js"
+  },
+  {
+    "revision": "682edddca3f6012c4544",
+    "url": "/tutorial-react/static/js/runtime-main.4b9bd8d2.js"
+  }
+]);
